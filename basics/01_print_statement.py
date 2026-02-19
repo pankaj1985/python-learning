e4893dbd0d6b2683c12b2660e377b2bbff1cbd13
root <root@LAPTOP-PHGFEHFR.localdomain>
2026-02-19 14:35:05 +0000

    Added print statment example

---
+++ basics/01_print_statement.py
@@ -0,0 +1,38 @@
+# This is a simple Python program that prints "Hello, World!" to the console.
+print("Hello, World!")
+
+
+# You can also print other messages by changing the text inside the quotes.
+print("Welcome to Python programming!")
+
+# You can print numbers as well.
+print(42)
+
+# You can store values in variables and print them.
+n=10
+print(n)
+
+# You can also print multiple items by separating them with commas.
+n1=10
+print("The value of n1 is:", n1)
+
+# You can use the format method to include variables in your print statements.
+n2=10
+print("The value of n2 is {}".format(n2))
+
+# You can perform calculations and print the results.
+a=10
+b=20
+sum = a+b
+print("The sum of {} and {} is {}".format(a,b, sum))
+
+# You can also use f-strings for a more concise way to include variables in your print statements.
+print(f"The sum of {a} and {b} is {sum}")
+
+# You can print multiple lines by using triple quotes.
+print("""This is multi line print statament
+you can write many lines of text here
+and it will all printed together.""")
+
+
+
