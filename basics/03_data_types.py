2b02630264c046c00a71daa6d28620eb0e69f59e
root <root@LAPTOP-PHGFEHFR.localdomain>
2026-02-19 17:30:00 +0000

    Added: Arithmethic practices and data type

---
+++ basics/03_data_types.py
@@ -0,0 +1,34 @@
+
+#name is a variable that holds the string value "Pankaj".
+name = "Pankaj"
+
+#status is a variable that holds the boolean value True.
+status = True
+
+#marks is a variable that holds the floating-point value 85.5.
+marks = 85.5
+
+# You can print the value of a variable by using the print function.
+print(f"The value of n is: {n}")
+
+print(f"The value of name is : {name}")
+
+print(f"The value of status is: {status}")   
+
+print(f"The value of marks is: {marks}") 
+
+#Displaying the type of variables using the type() function.
+print(f"The type of n is: {type(n)}") # Output: <class 'int'>
+print(f"The type of name is: {type(name)}") # Output: <class 'str'>
+print(f"The type of status is: {type(status)}") # Output: <class 'bool'>
+print(f"The type of marks is: {type(marks)}") # Output: <class 'float'>
+
+
+
+# You can also reassign a variable to a different value or type.
+n= "Hello" # Now n holds a string value instead of an integer.
+print(n)    # Output: Hello
+
+
+print(f"The value of n is now: {n} and its type is: {type(n)}")
+
---
+++ practice/02_arithmetic_operation.py
@@ -0,0 +1,48 @@
+a = 10
+b = 20
+
+# Addition
+sum = a + b
+print(f"Sum of {a} and {b} is {sum}")
+
+# Subtraction
+difference = a - b
+print(f"Difference of {a} and {b} is {difference}")
+
+# Multiplication
+product = a * b
+print(f"Multiplication of {a} and {b} is {product}")
+
+# Division
+quotient = a / b
+print(f"Division of {a} by {b} is {quotient}")
+
+# Modulus
+remainder = a % b
+print(f"Remainder when {a} is divided by {b} is {remainder}")
+
+# Floor Division
+floor_division = a // b
+print(f"Floor division of {a} by {b} is {floor_division}")
+
+# Exponentiation
+power = a ** b
+print(f"{a} raised to the power of {b} is {power}")
+
+# You can also perform operations with other data types, such as strings.
+str1 = "Hello"
+str2 = "World"
+# Concatenation
+concatenated_str = str1 + " " + str2
+print(f"Concatenation of '{str1}' and '{str2}' is: '{concatenated_str}'")
+
+# Repetition
+repeated_str = str1 * 3
+print(f"Repetition of '{str1}' three times is: '{repeated_str}'")
+
+# You can also perform operations with boolean values.
+bool1 = True   
+bool2 = False
+# Logical AND
+and_result = bool1 and bool2
+print(f"Logical AND of {bool1} and {bool2} is: {and_result}")
