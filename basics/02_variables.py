cb2605c788d1dcd17f4015322394e540128bb6f5
root <root@LAPTOP-PHGFEHFR.localdomain>
2026-02-19 14:50:12 +0000

    Added variable type and datatype example with type checking and reassignment

---
+++ basics/02_variables.py
@@ -0,0 +1,41 @@
+# In Python, you can assign values to variables using the equals sign (=).
+# The variable name can be any valid identifier, and it can hold different types of data, such as numbers, strings, 
+# or boolean values.
+
+# Here are some examples of variable assignments:
+#n is a variable that holds the integer value 10.
+n = 10 
+
+#name is a variable that holds the string value "Pankaj".
+name = "Pankaj"
+
+#status is a variable that holds the boolean value True.
+status = True
+
+#marks is a variable that holds the floating-point value 85.5.
+marks = 85.5
+
+# You can print the value of a variable by using the print function.
+print(f"The value of n is: {n}")
+
+print(f"The value of name is : {name}")
+
+print(f"The value of status is: {status}")   
+
+print(f"The value of marks is: {marks}") 
+
+#Displaying the type of variables using the type() function.
+print(f"The type of n is: {type(n)}") # Output: <class 'int'>
+print(f"The type of name is: {type(name)}") # Output: <class 'str'>
+print(f"The type of status is: {type(status)}") # Output: <class 'bool'>
+print(f"The type of marks is: {type(marks)}") # Output: <class 'float'>
+
+
+
+# You can also reassign a variable to a different value or type.
+n= "Hello" # Now n holds a string value instead of an integer.
+print(n)    # Output: Hello
+
+
+print(f"The value of n is now: {n} and its type is: {type(n)}")
+
