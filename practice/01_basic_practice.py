0e0ba3abf3aa01fa6bafcbabd4473828c8736621
root <root@LAPTOP-PHGFEHFR.localdomain>
2026-02-19 17:12:18 +0000

    Added practice program

---
+++ practice/01_basic_practice.py
@@ -0,0 +1,61 @@
+# 1.wap to add two numbers
+a = 10
+b = 20
+sum = a + b
+print(f"The sume of {a} and {b} is: {sum}")
+
+# 2.wap to find the area of a circle
+radius = 5
+pi = 3.14
+area_of_circle = pi*radius*radius
+print(f"The area of circle with radius {radius} is: {area_of_circle}")
+
+
+# 3.wap to find the area of a rectangle
+length = 10
+breadth = 5
+area_of_rectangle = length*breadth
+print(f"The area of rectangle with length {length} and breadth {breadth} is: {area_of_rectangle}")
+
+# 4.wap to find the area of a triangle
+base =10
+height =5
+area_of_triangle = 0.5*base*height
+print(f"The area of Triangle with base {base} and height {height} is: {area_of_triangle}")
+
+# 5.wap to find the area of a square
+side =5
+area_of_square = side*side
+print(f"Area of sqaure with side {side} is: {area_of_square}")
+
+# 6.wap to find the area of a trapezium
+base_1 = 10
+base_2 = 5
+height = 7
+area_of_trapezium = 0.5*(base_1+base_2)*height
+print(f"The area of trapezium with base_1 {base_1}, base_2 {base_2} and height {height} is: {area_of_trapezium}")
+
+# 7.wap to calculate the perimeter of circle
+radius = 2
+pi = 3.14
+perimeter_of_circle = 2*pi*radius
+print(f"The perimeter of circle with radius {radius} is: {perimeter_of_circle}")
+
+# 8.wap to calculate the perimeter of rectangle
+length =10
+breadth = 5
+perimeter_of_rectangle = 2*(length+breadth)
+print(f"The perimeter of rectangle with lenght {length} and breadth {breadth} is {perimeter_of_rectangle}")
+
+# 9.wap to calculate the perimeter of triangle
+side_1 = 3
+side_2 = 4
+side_3 = 5
+perimeter_of_triangle = side_1+side_2+side_3
+print(f"The perimeter of triangle with sides {side_1}, {side_2} and {side_3} is {perimeter_of_triangle}")
+
+# 10.wap to calculate the perimeter of square
+side =5
+perimeter_of_square = 4*side
+print(f"The perimeter of square with side {side} is: {perimeter_of_square}")
+
---
+++ practice/README.md
@@ -0,0 +1,6 @@
+# Practice Programs
+
+This folder contains small hands-on practice programs
+to strengthen understanding of Python concepts.
+
+These programs are written as part of daily coding practice.
